79a17879e3d86d98cd8220b137f52fb33fac1b33
Denick <1049485+apie@users.noreply.github.com>
2020-01-29 20:42:33 +0100

    Use json endpoint

--- afval.py
+++ afval.py
@@ -5,7 +5,6 @@
 #MIT
 
 import requests
-from bs4 import BeautifulSoup
 from icalendar import Event, Calendar
 from datetime import timedelta, datetime
 from os import path
@@ -18,21 +17,11 @@ from settings import postcode, huisnummer
 timezone = 'Europe/Amsterdam'
 
 
-def lees_website():
-    response = requests.get(f'https://www.mijnafvalwijzer.nl/nl/{postcode}/{huisnummer}/')
+def lees_json():
+    response = requests.get(f'https://json.mijnafvalwijzer.nl/?method=postcodecheck&postcode={postcode}&huisnummer={huisnummer}')
     response.raise_for_status()
-    soup = BeautifulSoup(response.content, 'lxml')
-    afvalkal = []
-    for t in ("pmd", "gft", "papier", "restafval"):
-        for e in soup.find_all(class_=t):
-            date_str, waste_type = e.text.strip().split('\n')
-            if date_str.lower() == t:
-                continue  # Not a date
-            date = dateparser.parse(date_str, settings={'TIMEZONE': timezone, 'RETURN_AS_TIMEZONE_AWARE': True})
-            afvalkal.append((date, waste_type))
-
-    return afvalkal
-
+    pickupdates = response.json()['data']['ophaaldagen']['data']
+    return map(lambda d: (dateparser.parse(d['date'], settings={'TIMEZONE': timezone, 'RETURN_AS_TIMEZONE_AWARE': True}), d['type']), pickupdates)
 
 def schrijf_ical(afvalkal):
     cal = Calendar()
@@ -42,7 +31,7 @@ def schrijf_ical(afvalkal):
     now = datetime.now(pytz.timezone(timezone))
     for date, waste_type in sorted(afvalkal):
         event = Event()
-        event.add('summary', f'Afvalkalender {waste_type}')
+        event.add('summary', f'Afvalkalender {waste_type.capitalize()}')
         event.add('dtstart', date+timedelta(seconds=60*60*7)) # 07:00
         event.add('dtend',   date+timedelta(seconds=60*60*9)) # 09:00
         event.add('dtstamp', now) 
@@ -53,4 +42,4 @@ def schrijf_ical(afvalkal):
 
 
 if __name__ == "__main__":
-    schrijf_ical(lees_website())
+    schrijf_ical(lees_json())
